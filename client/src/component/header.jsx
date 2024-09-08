eb340244cc97d4e1e60c1dafef458265478490ed
ritwik-raj30 <btech10199.22@bitmesra.ac.in>
2024-09-08 15:06:56 +0530

    minor frontend chnage

--- client/src/component/header.jsx
+++ client/src/component/header.jsx
@@ -7,22 +7,22 @@ const Header = () => {
       id="header"
       className="bg-gradient-to-r from-blue-99 to-white text-blue-900 py-16 px-4 shadow-lg overflow-hidden mt-20"
     >
-      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
+      <div className="container mx-auto flex flex-col lg:flex-row-reverse items-center justify-between">
         {/* Text Section */}
-        <div className="text-center lg:text-left mb-12 lg:mb-0 lg:w-2/3">
+        <div className="text-center lg:text-right mb-12 lg:mb-0 lg:w-2/3">
           <div className="inline-block">
             <h1 className="text-2xl md:text-7xl lg:text-9xl font-bold mb-2 animate-slide-right">
               AARCO
             </h1>
             <h2 className="text-3xl md:text-xl lg:text-3xl font-light animate-slide-left whitespace-nowrap overflow-hidden text-ellipsis">
-              ASSOCIATION OF ATOMIC ENERGY OFFICERS
+            Association of Alumni Retirees of Our Community
             </h2>
           </div>
-          <div className="flex flex-col md:flex-row items-center justify-center lg:justify-start space-y-6 md:space-y-0 md:space-x-8 text-xl"></div>
+          <div className="flex flex-col md:flex-row items-center justify-center lg:justify-end space-y-6 md:space-y-0 md:space-x-8 text-xl"></div>
         </div>
 
         {/* Logo Section */}
-        <div className="flex-shrink-0 lg:w-1/3 flex justify-center lg:justify-end items-center animate-slide-up">
+        <div className="flex-shrink-0 lg:w-1/3 flex justify-center lg:justify-start items-center animate-slide-up">
           <img
             src={logos}
             alt="AARCO Logo"
